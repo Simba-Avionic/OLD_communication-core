2a575aa630baaed39ce8f59a9a17802214c65469
Mateusz <matikrajek42@gmail.com>
2023-11-17 10:58:24 +0100

    makr_implementation_udpSocket

--- sockets/udp_socket.h
+++ sockets/udp_socket.h
@@ -10,13 +10,32 @@
  */
 #ifndef COMUNICATION_CORE_SOCKETS_UDP_SOCKET_H_
 #define COMUNICATION_CORE_SOCKETS_UDP_SOCKET_H_
-#include "communication-core/sockets/Isocket.h"
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <thread>
+#include <memory>
+
+#include "sockets/Isocket.h"
 namespace simba {
 namespace com {
 namespace soc {
 class UdpSocket : public ISocket {
  private:
-  /* data */
+  int server_sock,len,rc;
+  int bytes_rec =0;
+  struct sockaddr_in server_sockaddr,peer_sock;
+  
+  std::unique_ptr<std::thread> rx_thread;
+  void Loop();
+  RXCallback callback_;
  public:
   /**
    * @brief Socket init function
